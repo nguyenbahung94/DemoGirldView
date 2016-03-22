cb2e70b036b504064fde32b50fa2116c0fa56b5a
nguyenbahung94 <mr.hungcity@gmail.com>
2016-03-22 10:36:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/examplegirldview/ImageAdapter.java
@@ -0,0 +1,50 @@
+package com.example.android.examplegirldview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by everything on 3/9/2016.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mcontext;
+    private Integer[] mThumbids={R.drawable.sample_0,R.drawable.sample_1,R.drawable.sample_2
+    ,R.drawable.sample_3,R.drawable.sample_4,R.drawable.sample_5,R.drawable.sample_6,R.drawable.sample_7};
+    public ImageAdapter(Context c){
+        mcontext=c;
+
+    }
+    @Override
+    public int getCount() {
+        return mThumbids.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView==null){
+            imageView=new ImageView(mcontext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85,85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+        }else{
+            imageView=(ImageView)convertView;
+        }
+        imageView.setImageResource(mThumbids[position]);
+        return imageView;
+    }
+}
